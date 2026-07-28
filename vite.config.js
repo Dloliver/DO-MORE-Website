c40492b282374fe727691e8da88dd1de486d59fe
Dennis Oliver <91744578+Dloliver@users.noreply.github.com>
2026-07-28 08:52:48 -0400

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
-  // Relative asset URLs work at both:
-  // https://dloliver.github.io/DO-MORE-Website/
-  // and the future https://domoreatl.com/
-  base: './',
+  base: '/DO-MORE-Website/',
 })
