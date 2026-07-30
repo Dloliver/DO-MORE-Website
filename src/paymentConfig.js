612fd3461b4ac6fb6155781dc2c3865b36eada70
Dennis Oliver <91744578+Dloliver@users.noreply.github.com>
2026-07-30 01:26:55 -0400

    Activate strategy session payments

---
+++ src/paymentConfig.js
@@ -0,0 +1,8 @@
+// Do More ATL payment configuration.
+//
+// Keep STRATEGY_PAYMENTS_ACTIVE set to false while Stripe shows the
+// Payment Link as Paused. Change it to true after Stripe marks the link Active.
+export const STRATEGY_PAYMENT_URL =
+  'https://buy.stripe.com/5kQ6oJ9gMaPGboideWbo400'
+
+export const STRATEGY_PAYMENTS_ACTIVE = false
